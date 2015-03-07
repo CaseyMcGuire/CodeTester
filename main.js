5317db36268fe84b987be9fa6eed38438271685a
Casey McGuire <caseymcguire90@gmail.com>
2015-03-07 12:12:30 -0800

    Can now retrieve ungraded submissions via GET request

--- main.js
+++ main.js
@@ -3,55 +3,17 @@ if(require.main === module){
 }
 
 function main(){
-    var http = require('http');
 
-    var options = {
-	hostname: 'localhost',
-	path: '/submissions/get_ungraded',
-	port: 3000,
-	headers: {
-	    'Content-Type': 'application/json'
+    var request = require('request');
+    request({
+	uri: 'http://localhost:3000/submissions/get_ungraded',
+	json: true
+    }, function(error, response, body){
+	if(error){
+	    console.log(error);
+	    console.log(response);
 	}
-
-    }
-
-    http.get(options, function(res){
-//	console.log(res);
-	res.setEncoding('utf8');
-	var body = '';
-	res.on('data', function(chunk){
-	    body += chunk;
-
-	});
-	res.on('end', function(){
-	    console.log(body);
-	    var obj = JSON.parse(body);
-	    console.log(obj);
-	});
-
-    });
-    
-    /*
-    var req = http.request(options, function(res){
-	res.setEncoding('utf8');
-	res.on('data', function(chunk){
-	    console.log('Body: ' + chunk);
-	});
-
+	else console.log(body);
     });
 
-    req.on('error', function(e){
-	console.log("There was a problem: " + e.message);
-    });
-    req.end();
-*/
-/*
-    http.get("http://localhost:3000/problems", function(res){
-	console.log("got response: " + res.statusCode);
-//	console.log(res);
-    }).on('error', function(e){
-	console.log("got error: " + e.message);
-    });
-
-*/
 }
