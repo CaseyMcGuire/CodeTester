e6d6789a7b5dc970d7ecf2bc8b9cf5e7c58b75e3
Casey McGuire <casey.jay.mcguire@gmail.com>
2015-03-29 17:37:28 -0700

    Getting and posting submissions now kinda works

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 if(require.main === module){
+    //TODO: mke sure pass GET and POST uri
     main();
 }
 
 function main(){
-
     var request = require('request');
+    getUngradedSubmission(request, function(error, body){
+	console.log(body);
+	request({
+	    method: 'POST',
+	    uri: 'http://localhost:3000/submission/update/1',
+	    json: true,
+	    body: JSON.stringify({"hello" : "world"})
+	}, function(error, response, body){
+	    if(error) console.log(error);
+	    else console.log(body);
+	});
+    });
+}
+
+/*
+  
+ */
+function getUngradedSubmission(request, callback){
     request({
-	uri: 'http://localhost:3000/submissions/get_ungraded',
+	uri: 'http://localhost:3000/submission/get_ungraded',
 	json: true
     }, function(error, response, body){
-	if(error){
-	    console.log(error);
-	    console.log(response);
-	}
-	else console.log(body);
+	if(error) callback(error);
+	else callback(null, body);//console.log(body);
     });
-
 }
