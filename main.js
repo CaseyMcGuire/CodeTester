8a7c6c4c56ede025d80a86d323fdf6c11757ae7c
Casey McGuire <casey.jay.mcguire@gmail.com>
2015-03-29 20:35:31 -0700

    Moved some things around

--- main.js
+++ main.js
@@ -7,17 +7,10 @@ function main(){
     var request = require('request');
     getUngradedSubmission(request, function(error, body){
 	console.log(body);
-	request({
-	    method: 'POST',
-	    uri: 'http://localhost:3000/submission/update/1',
-	    json: true,
-	    body: JSON.stringify({"hello" : "world"})
-	}, function(error, response, body){
-	    if(error) console.log(error);
-	    else console.log(body);
-	});
     });
 }
+
+
 
 /*
   
@@ -29,5 +22,17 @@ function getUngradedSubmission(request, callback){
     }, function(error, response, body){
 	if(error) callback(error);
 	else callback(null, body);//console.log(body);
+    });
+}
+
+function postResult(request, submission_id, request, callback){
+    request({
+	method: 'POST',
+	uri: 'http://localhost:3000/submission/update/' + submission_id,
+	json: true,
+	body: {"result" : "fail"}
+    }, function(error, response, body){
+	if(error) console.log(error);
+	else console.log(body);
     });
 }
